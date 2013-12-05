6836e7a5e64417dc8ad62b346610d1f50f12641b
Tim Nieradzik <tim.nieradzik@gmx.de>
2013-12-05 16:25:33 +0100

    Refactor test suite.

--- src/org/oopsc/TestSuite.scala
+++ src/org/oopsc/TestSuite.scala
@@ -3,77 +3,36 @@ package org.oopsc
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.fail
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.InputStream
-import java.io.UnsupportedEncodingException
+import java.io._
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
-import java.nio.file.FileVisitResult
-import java.nio.file.Files
-import java.nio.file.NoSuchFileException
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.nio.file.SimpleFileVisitor
-import java.nio.file.attribute.BasicFileAttributes
-import java.util.ArrayList
+import java.nio.file.{NoSuchFileException, Files, Paths}
 import java.util.Collection
-import java.util.Collections
-import java.util.Comparator
-import java.util.List
 import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.tree.ParseTree
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
-import org.oopsvm.AsmException
 import org.oopsvm.Assembler
-import org.oopsvm.VMException
 import org.oopsvm.VirtualMachine
 
 @RunWith(classOf[Parameterized]) object TestSuite {
   def readFile(path: String, encoding: Charset): String = {
-    val encoded: Array[Byte] = Files.readAllBytes(Paths.get(path))
+    val encoded = Files.readAllBytes(Paths.get(path))
     return encoding.decode(ByteBuffer.wrap(encoded)).toString
+  }
+
+  def recursiveListFiles(f: File): Array[File] = {
+    val these = f.listFiles
+    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
   }
 
   @Parameters(name = "{0}")
   def data: Collection[Array[AnyRef]] = {
-    val data: List[Array[AnyRef]] = new ArrayList[Array[AnyRef]]
-
-    try {
-      Files.walkFileTree(Paths.get("tests/"), new SimpleFileVisitor[Path] {
-        override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
-          if (file.toString.endsWith(".oops")) {
-            data.add(Array[AnyRef](file.toString))
-          }
-
-          return FileVisitResult.CONTINUE
-        }
-
-        override def visitFileFailed(file: Path, exc: IOException): FileVisitResult = {
-          return FileVisitResult.CONTINUE
-        }
-      })
-    } catch {
-      case e: IOException => {
-        e.printStackTrace
-      }
-    }
-
-    Collections.sort(data, new Comparator[Array[AnyRef]] {
-      def compare(o: Array[AnyRef], o2: Array[AnyRef]): Int = {
-        return (o(0).asInstanceOf[String]).compareTo(o2(0).asInstanceOf[String])
-      }
-    })
-
-    return data
+    val files = recursiveListFiles(new File("tests/")).map(_.toString).filter(_.endsWith(".oops")).sorted.map(Array[AnyRef](_))
+    return scala.collection.JavaConversions.mutableSeqAsJavaList(files)
   }
 }
 
@@ -82,11 +41,9 @@ class TestSuite(var path: String) {
   private var p: Program = null
 
   def runVM(asm: String, input: String): String = {
-    val asmStream: InputStream = new ByteArrayInputStream(asm.getBytes("UTF-8"))
-
-    val vm: VirtualMachine = new VirtualMachine(new Assembler(false, false).assemble(asmStream), new Array[Int](8), false, false, false, false, false, false, false)
-
-    val output: ByteArrayOutputStream = new ByteArrayOutputStream
+    val asmStream = new ByteArrayInputStream(asm.getBytes("UTF-8"))
+    val vm = new VirtualMachine(new Assembler(false, false).assemble(asmStream), new Array[Int](8), false, false, false, false, false, false, false)
+    val output = new ByteArrayOutputStream
 
     vm.setStreams(new ByteArrayInputStream(input.getBytes), output)
     vm.run
@@ -96,15 +53,15 @@ class TestSuite(var path: String) {
 
   @Test
   def testGrammar {
-    val stream: FileInputStream = new FileInputStream(this.path)
-    val input: ANTLRInputStream = new ANTLRInputStream(stream)
-    val lexer: GrammarLexer = new GrammarLexer(input)
-    val tokens: CommonTokenStream = new CommonTokenStream(lexer)
-    val parser: GrammarParser = new GrammarParser(tokens)
+    val stream = new FileInputStream(this.path)
+    val input = new ANTLRInputStream(stream)
+    val lexer = new GrammarLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new GrammarParser(tokens)
 
     parser.program
 
-    val supposedToFail: Boolean = this.path.contains("_se")
+    val supposedToFail = this.path.contains("_se")
 
     if (supposedToFail) {
       assertNotNull(parser.getNumberOfSyntaxErrors)
@@ -113,41 +70,40 @@ class TestSuite(var path: String) {
     }
   }
 
-  @Test def testVisitor {
-    val supposedToFail: Boolean = this.path.contains("_se")
+  @Test
+  def testVisitor {
+    val supposedToFail = this.path.contains("_se")
 
     if (supposedToFail) {
       /* Skip as we cannot use the visitor on malformed code samples. */
       return
     }
 
-    val stream: FileInputStream = new FileInputStream(this.path)
-    val input: ANTLRInputStream = new ANTLRInputStream(stream)
-    val lexer: GrammarLexer = new GrammarLexer(input)
-    val tokens: CommonTokenStream = new CommonTokenStream(lexer)
-    val parser: GrammarParser = new GrammarParser(tokens)
+    val stream = new FileInputStream(this.path)
+    val input = new ANTLRInputStream(stream)
+    val lexer = new GrammarLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new GrammarParser(tokens)
 
-    val tree: ParseTree = parser.program
+    val tree = parser.program
 
-    val visitor: ProgramVisitor = new ProgramVisitor(new Program)
+    val visitor = new ProgramVisitor(new Program)
     visitor.visit(tree)
   }
 
   /**
    * Performs syntax and context analysis. Also tests the code generation.
-   *
-   * @throws Exception
    */
-  @Test def testFile {
-    val supposedToFail: Boolean = this.path.contains("_se")
+  @Test
+  def testFile {
+    val supposedToFail = this.path.contains("_se")
     val pathExpected = this.path.substring(0, this.path.length - 5) + ".out"
-    var expected = ""
-
-    try {
-      expected = TestSuite.readFile(pathExpected, StandardCharsets.UTF_8)
-    } catch {
-      case e: NoSuchFileException => { }
-    }
+    val expected =
+       try {
+         TestSuite.readFile(pathExpected, StandardCharsets.UTF_8)
+       } catch {
+         case e: NoSuchFileException => ""
+       }
 
     try {
       this.p = new SyntaxAnalysis(this.path, false).parse
@@ -155,21 +111,17 @@ class TestSuite(var path: String) {
       this.p.printTree
 
       /* Test code generation. */
-      val stream: ByteArrayOutputStream = new ByteArrayOutputStream
-      val code: CodeStream = CodeStream.apply(stream)
+      val stream = new ByteArrayOutputStream
+      val code = CodeStream.apply(stream)
       this.p.generateCode(code, 1000, 1000)
       val asm = stream.toString("UTF-8")
 
       System.err.println(asm)
 
       /* Run the VM twice with different inputs. */
-      val output: String = this.runVM(asm, "abc") + this.runVM(asm, "xyz")
+      val output = this.runVM(asm, "abc") + this.runVM(asm, "xyz")
       assertEquals(expected, output)
     } catch {
-      case e: UnsupportedEncodingException => {
-        fail
-      }
-
       case e: CompileException => {
         if (supposedToFail) {
           System.err.println(e.getMessage)
